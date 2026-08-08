d4f9a06d972cab865220ce70534572329c02e0a7
Harshit Sonawala <hssonawala@gmail.com>
2026-08-08 18:57:36 +0530

    chore: Updated to NativeWind v5 & Tailwind v4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -20,11 +20,11 @@ export default function HomeScreen() {
     <ScrollView className="flex-1 bg-black">
       <SafeAreaView className="flex-1 px-6 py-6 gap-y-6">
         <View className="gap-y-1">
+          <Text className="text-base text-neutral-400 uppercase">
+            Welcome Back
+          </Text>
           <Text className="text-3xl font-semibold text-white">
             Keystone Work Clock
-          </Text>
-          <Text className="text-base text-neutral-400">
-            Track daily work hours
           </Text>
         </View>
 
